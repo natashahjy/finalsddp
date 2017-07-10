dee358f4b64ce799c232969db2430e85c7d84124
ITP312 <itp312@l632-43.local>
2017-07-10 11:34:01 +0800

    July 04 update

---
+++ ShareATextbook/PreviewViewController.swift
@@ -0,0 +1,58 @@
+//
+//  PreviewViewController.swift
+//  ShareATextbook
+//
+//  Created by ITP312 on 10/7/17.
+//  Copyright © 2017 natashahjy. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController {
+
+    var barcode: String!
+    var bookTitle: String!
+    var condition: String!
+    var category : String!
+    
+    @IBOutlet weak var barcodeLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var tagLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // barcodeLabel.text = barcode
+    
+    
+    func prepareForSegue(segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Upload" {
+            let uploadVC = segue.destination as! UploadViewController
+            uploadVC.barcode = barcode
+            uploadVC.bookTitle = bookTitle
+            uploadVC.condition = condition
+            uploadVC.category = category
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
